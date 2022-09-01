cedbea23b2c352ae271ea25032d42280e5d5d767
angusmackwalker <angusw@olympiccascadeinc.com>
2022-09-01 10:50:34 -0400

    Finished L2 and Code Challenge.

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+            description: 'Feet warmers, comes in pairs. To be worn inside shoes.'
+        }
+    }
+});
+
+// const product = 'Socks'
