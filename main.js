a2809f0ad38b547db95e8b514d2152a34426a38a
angusmackwalker <angusw@olympiccascadeinc.com>
2022-09-01 14:54:12 -0400

    Added size array for coding challenge

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id:2334, color:'green'},
+                {id:2335, color:'blue'},
+            ],
+            sizes:['small', 'medium', 'large']
+
         }
     }
 })
