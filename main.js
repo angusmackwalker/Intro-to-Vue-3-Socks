c71dccf0041bb693d42545e41d1e5c464718b28f
angusmackwalker <angusw@olympiccascadeinc.com>
2022-09-01 11:13:50 -0400

    Completed L3 with v-bind and a href Code Challenge

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            imagePath: './assets/images/socks_green.jpg',
+            blueSox: './assets/images/socks_blue.jpg',
+            goo:'http://www.google.com'
         }
     }
 })
